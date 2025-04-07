fdde56589521340952c3f4b433fbbd54b5222b76
egarrido12 <111029178+egarrido12@users.noreply.github.com>
2025-04-06 23:40:08 -0500

    Create Serie.js

---
+++ Series_2/Serie.js
@@ -0,0 +1,10 @@
+export class Serie {
+  constructor(id, name, channel, seasons, description, image) {
+    this.id = id;
+    this.name = name;
+    this.channel = channel;
+    this.seasons = seasons;
+    this.description = description;
+    this.image = image;
+  }
+}
