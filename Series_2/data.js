f07a8911247d16f641869c9bbb388e239bc60cbd
egarrido12 <111029178+egarrido12@users.noreply.github.com>
2025-04-06 23:39:27 -0500

    Create data.js

---
+++ Series_2/data.js
@@ -0,0 +1,20 @@
+import { Serie } from "./Serie.js";
+
+export const series = [
+  new Serie(1, "Breaking Bad", "AMC", 5,
+    "Un profesor de química con cáncer se convierte en fabricante de metanfetamina.",
+    "https://upload.wikimedia.org/wikipedia/en/6/61/Breaking_Bad_title_card.png"
+  ),
+  new Serie(2, "Orange Is the New Black", "Netflix", 6,
+    "Una mujer es enviada a prisión por un crimen del pasado y debe adaptarse a la vida carcelaria.",
+    "https://upload.wikimedia.org/wikipedia/en/3/3b/Orange_Is_the_New_Black_title_card.png"
+  ),
+  new Serie(3, "Game of Thrones", "HBO", 8,
+    "Nobleza, dragones y traición en la lucha por el Trono de Hierro.",
+    "https://upload.wikimedia.org/wikipedia/en/d/d8/Game_of_Thrones_title_card.jpg"
+  ),
+  new Serie(4, "The Big Bang Theory", "CBS", 12,
+    "Un grupo de científicos socialmente torpes navega por la vida y las relaciones.",
+    "https://upload.wikimedia.org/wikipedia/en/7/7e/The_Big_Bang_Theory_title_card.png"
+  )
+];
